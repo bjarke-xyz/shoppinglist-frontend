a5ed972f44affdb99a09d873492995081ed7131d
bjarkt <bjarke.tobiesen@gmail.com>
2021-07-24 19:40:11 +0200

    uri encode redirect uri

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,4 +2,6 @@ export const SSO_URL = process.env.REACT_APP_SSO_URL;
 export const API_URL = process.env.REACT_APP_API_URL;
 export const FRONTEND_URL = process.env.REACT_APP_FRONTEND_URL;
 
-export const LOGIN_URL = `${SSO_URL}/auth/realms/shoppinglist/protocol/openid-connect/auth?response_type=code&client_id=App&scope=openid&redirect_uri=${FRONTEND_URL}/home`;
+export const LOGIN_URL = `${SSO_URL}/auth/realms/shoppinglist/protocol/openid-connect/auth?response_type=code&client_id=App&scope=openid&redirect_uri=${encodeURI(
+  FRONTEND_URL ?? ""
+)}/home`;
