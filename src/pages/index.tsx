d81f797c371d6eb857df0c007ef71ef12a674d34
bjarkt <688414+bjarkt@users.noreply.github.com>
2022-06-06 12:53:35 +0200

    landing page, pwa files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,63 @@
-import { Button, Container } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Link,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Image,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
+import NextLink from "next/link";
 
 const Home: NextPage = () => {
   return (
-    <Container>
-      <div>
-        <p>Wow! Landing page!</p>
-        <Button color={"green.400"}>hej</Button>
-        <Link href={"/app"}>App</Link>
-      </div>
-    </Container>
+    <Box bgImage="/charlie-brown.svg">
+      <Container>
+        <Flex flexDir="column" height="100vh">
+          <Box
+            bgColor="white"
+            padding="4"
+            mt="4"
+            boxShadow="md"
+            borderRadius="md"
+          >
+            <Flex flexDir="column" justify="center" align="center">
+              <Heading mr="4">Simple Shopping List</Heading>
+              <Flex justify="right" width="100%">
+                <NextLink href="/app" passHref>
+                  <Link>
+                    Go to app <ExternalLinkIcon mx="2px" />
+                  </Link>
+                </NextLink>
+              </Flex>
+            </Flex>
+            <p>
+              Simple shopping list application, accessible on web and mobile
+            </p>
+            <Wrap mt="4" justify="center">
+              <WrapItem>
+                <Image
+                  alt="Screenshot of application on mobile device"
+                  maxHeight="300px"
+                  src="/screenshot.png"
+                ></Image>
+              </WrapItem>
+              <WrapItem>
+                <Image
+                  alt="Screenshot of application"
+                  maxHeight="300px"
+                  src="/screenshot_landscape.png"
+                ></Image>
+              </WrapItem>
+            </Wrap>
+          </Box>
+        </Flex>
+      </Container>
+    </Box>
   );
 };
 
